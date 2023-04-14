9e0d53c8c89d6b00e1cf0331ce4012e8c03a8bbe
Jason Yang <Jasonyang602@gmail.com>
2023-04-14 19:47:09 -0400

    season 0 matchups

--- vd2l/src/Pages/Season.jsx
+++ vd2l/src/Pages/Season.jsx
@@ -8,47 +8,88 @@ import { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { Link } from "react-router-dom";
 import { onSnapshot } from "firebase/firestore";
-import { getDocs } from 'firebase/firestore';
-import { signupsRef, teamsRef } from '../firebase';
+import { getDocs } from "firebase/firestore";
+import { signupsRef, teamsRef } from "../firebase";
 
 export default function Season() {
-	
 	let playerData;
 	let teamsData;
 	const [tabSelected, setTabSelected] = useState(1);
 	const [players, setPlayers] = useState([]);
 	const [teams, setTeams] = useState([]);
-	const ranks = ["Iron I", "Iron II", "Iron III", "Bronze I", "Bronze II", "Bronze III", "Silver I", "Silver II", "Silver III", "Gold I", "Gold II", "Gold III", "Platinum I", "Platinum II", "Platinum III", "Diamond I", "Diamond II", "Diamond III"];
+	const [teamInfo, setTeamInfo] = useState([]);
+	const ranks = [
+		"Iron I",
+		"Iron II",
+		"Iron III",
+		"Bronze I",
+		"Bronze II",
+		"Bronze III",
+		"Silver I",
+		"Silver II",
+		"Silver III",
+		"Gold I",
+		"Gold II",
+		"Gold III",
+		"Platinum I",
+		"Platinum II",
+		"Platinum III",
+		"Diamond I",
+		"Diamond II",
+		"Diamond III",
+	];
 	useEffect(() => {
-
 		getDocs(signupsRef)
-		.then(snapshot => {
-			playerData = snapshot.docs.map(doc => {
-				return {
-					...doc.data()
-				}
+			.then((snapshot) => {
+				playerData = snapshot.docs.map((doc) => {
+					return {
+						...doc.data(),
+					};
+				});
+				setPlayers(
+					playerData.map((player, index) => (
+						<SignupRow
+							key={index}
+							row={index + 1}
+							name={player.ign}
+							rank={ranks[player.rank - 1]}
+							statement={player.statement}
+						/>
+					))
+				);
 			})
-			setPlayers(playerData.map((player, index) => <SignupRow key={index} row={index+1} name={player.ign} rank={ranks[player.rank-1]} statement={player.statement}/>))
-		})
-		.catch(err => {
-			console.log(`%cError: ${err.message}`, "color:red");
-		})
-
+			.catch((err) => {
+				console.log(`%cError: ${err.message}`, "color:red");
+			});
 
 		getDocs(teamsRef)
-		.then(snapshot => {
-			teamsData = snapshot.docs.map(doc => {
-				return {
-					...doc.data()
-				}
+			.then((snapshot) => {
+				teamsData = snapshot.docs.map((doc) => {
+					return {
+						...doc.data(),
+					};
+				});
+				setTeamInfo(teamsData);
+				const sortedTeamsData = teamsData.sort(
+					(a, b) => b.wins - a.wins
+				);
+				setTeams(
+					sortedTeamsData.map((team, index) => (
+						<TeamsRow
+							key={index}
+							row={index + 1}
+							team={team.name}
+							record={`${team.wins} - ${team.losses}`}
+							captain={team.captain}
+							logo={team.logo}
+						/>
+					))
+				);
+				console.log(teamsData);
 			})
-			const sortedTeamsData = teamsData.sort((a, b) => b.wins - a.wins);
-			setTeams(sortedTeamsData.map((team, index) => <TeamsRow key={index} row={index+1} team={team.name} record={`${team.wins} - ${team.losses}`} captain={team.captain} logo={team.logo} />))
-		})
-		.catch(err => {
-			console.log(`%cError: ${err.message}`, "color:red");
-		})
-
+			.catch((err) => {
+				console.log(`%cError: ${err.message}`, "color:red");
+			});
 	}, []);
 
 	function toggleTab(tab) {
@@ -128,9 +169,7 @@ export default function Season() {
 								{/* </div> */}
 							</tr>
 						</thead>
-						<tbody>
-							{players}
-						</tbody>
+						<tbody>{players}</tbody>
 					</table>
 				</motion.div>
 			)}
@@ -149,9 +188,7 @@ export default function Season() {
 								<th>Record ( W - L )</th>
 							</tr>
 						</thead>
-						<tbody>
-							{teams}
-						</tbody>
+						<tbody>{teams}</tbody>
 					</table>
 				</motion.div>
 			)}
@@ -161,14 +198,627 @@ export default function Season() {
 					initial={{ opacity: 0 }}
 					animate={{ opacity: 1 }}
 				>
-					<table className="styled-table">
+					<table className="styled-table matchup">
 						<thead>
 							<tr className="table-header">
-								<th className="hometeam week-title">Week 0</th>
+								<th className="hometeam week-title">Week 1</th>
+								<th className="score">Score</th>
 								<th className="awayteam"></th>
 							</tr>
 						</thead>
-						<tbody>  {/*teams*/}  </tbody>
+						<tbody>
+							<tr>
+								<td className="hometeam">
+									{teamInfo[4].name +
+										" (" +
+										teamInfo[4].wins +
+										" - " +
+										teamInfo[4].losses +
+										")"}
+								</td>
+								<td className="score">0 - 0</td>
+								<td className="awayteam">
+									{teamInfo[0].name +
+										" (" +
+										teamInfo[0].wins +
+										" - " +
+										teamInfo[0].losses +
+										")"}
+								</td>
+							</tr>
+							<tr>
+								<td className="hometeam">
+									{teamInfo[6].name +
+										" (" +
+										teamInfo[6].wins +
+										" - " +
+										teamInfo[6].losses +
+										")"}
+								</td>
+								<td className="score">0 - 0</td>
+								<td className="awayteam">
+									{teamInfo[5].name +
+										" (" +
+										teamInfo[5].wins +
+										" - " +
+										teamInfo[5].losses +
+										")"}
+								</td>
+							</tr>
+							<tr>
+								<td className="hometeam">
+									{teamInfo[7].name +
+										" (" +
+										teamInfo[7].wins +
+										" - " +
+										teamInfo[7].losses +
+										")"}
+								</td>
+								<td className="score">0 - 0</td>
+								<td className="awayteam">
+									{teamInfo[2].name +
+										" (" +
+										teamInfo[2].wins +
+										" - " +
+										teamInfo[2].losses +
+										")"}
+								</td>
+							</tr>
+							<tr>
+								<td className="hometeam">
+									{teamInfo[3].name +
+										" (" +
+										teamInfo[3].wins +
+										" - " +
+										teamInfo[3].losses +
+										")"}
+								</td>
+								<td className="score">0 - 0</td>
+								<td className="awayteam">
+									{teamInfo[1].name +
+										" (" +
+										teamInfo[1].wins +
+										" - " +
+										teamInfo[1].losses +
+										")"}
+								</td>
+							</tr>
+						</tbody>
+					</table>
+
+					{/*------------------------------------------ WEEK 2 ---------------------------------------------------------------------*/}
+					<table className="styled-table matchup">
+						<thead>
+							<tr className="table-header">
+								<th className="hometeam week-title">Week 2</th>
+								<th className="score">Score</th>
+								<th className="awayteam"></th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td className="hometeam">
+									{teamInfo[6].name +
+										" (" +
+										teamInfo[6].wins +
+										" - " +
+										teamInfo[6].losses +
+										")"}
+								</td>
+								<td className="score">0 - 0</td>
+								<td className="awayteam">
+									{teamInfo[4].name +
+										" (" +
+										teamInfo[4].wins +
+										" - " +
+										teamInfo[4].losses +
+										")"}
+								</td>
+							</tr>
+							<tr>
+								<td className="hometeam">
+									{teamInfo[7].name +
+										" (" +
+										teamInfo[7].wins +
+										" - " +
+										teamInfo[7].losses +
+										")"}
+								</td>
+								<td className="score">0 - 0</td>
+								<td className="awayteam">
+									{teamInfo[5].name +
+										" (" +
+										teamInfo[5].wins +
+										" - " +
+										teamInfo[5].losses +
+										")"}
+								</td>
+							</tr>
+							<tr>
+								<td className="hometeam">
+									{teamInfo[3].name +
+										" (" +
+										teamInfo[3].wins +
+										" - " +
+										teamInfo[3].losses +
+										")"}
+								</td>
+								<td className="score">0 - 0</td>
+								<td className="awayteam">
+									{teamInfo[0].name +
+										" (" +
+										teamInfo[0].wins +
+										" - " +
+										teamInfo[0].losses +
+										")"}
+								</td>
+							</tr>
+							<tr>
+								<td className="hometeam">
+									{teamInfo[1].name +
+										" (" +
+										teamInfo[1].wins +
+										" - " +
+										teamInfo[1].losses +
+										")"}
+								</td>
+								<td className="score">0 - 0</td>
+								<td className="awayteam">
+									{teamInfo[2].name +
+										" (" +
+										teamInfo[2].wins +
+										" - " +
+										teamInfo[2].losses +
+										")"}
+								</td>
+							</tr>
+						</tbody>
+					</table>
+
+					{/*------------------------------------------ WEEK 3 ---------------------------------------------------------------------*/}
+					<table className="styled-table matchup">
+						<thead>
+							<tr className="table-header">
+								<th className="hometeam week-title">Week 3</th>
+								<th className="score">Score</th>
+								<th className="awayteam"></th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td className="hometeam">
+									{teamInfo[6].name +
+										" (" +
+										teamInfo[6].wins +
+										" - " +
+										teamInfo[6].losses +
+										")"}
+								</td>
+								<td className="score">0 - 0</td>
+								<td className="awayteam">
+									{teamInfo[2].name +
+										" (" +
+										teamInfo[2].wins +
+										" - " +
+										teamInfo[2].losses +
+										")"}
+								</td>
+							</tr>
+							<tr>
+								<td className="hometeam">
+									{teamInfo[0].name +
+										" (" +
+										teamInfo[0].wins +
+										" - " +
+										teamInfo[0].losses +
+										")"}
+								</td>
+								<td className="score">0 - 0</td>
+								<td className="awayteam">
+									{teamInfo[1].name +
+										" (" +
+										teamInfo[1].wins +
+										" - " +
+										teamInfo[1].losses +
+										")"}
+								</td>
+							</tr>
+							<tr>
+								<td className="hometeam">
+									{teamInfo[3].name +
+										" (" +
+										teamInfo[3].wins +
+										" - " +
+										teamInfo[3].losses +
+										")"}
+								</td>
+								<td className="score">0 - 0</td>
+								<td className="awayteam">
+									{teamInfo[5].name +
+										" (" +
+										teamInfo[5].wins +
+										" - " +
+										teamInfo[5].losses +
+										")"}
+								</td>
+							</tr>
+							<tr>
+								<td className="hometeam">
+									{teamInfo[4].name +
+										" (" +
+										teamInfo[4].wins +
+										" - " +
+										teamInfo[4].losses +
+										")"}
+								</td>
+								<td className="score">0 - 0</td>
+								<td className="awayteam">
+									{teamInfo[7].name +
+										" (" +
+										teamInfo[7].wins +
+										" - " +
+										teamInfo[7].losses +
+										")"}
+								</td>
+							</tr>
+						</tbody>
+					</table>
+
+					{/*------------------------------------------ WEEK 4 ---------------------------------------------------------------------*/}
+					<table className="styled-table matchup">
+						<thead>
+							<tr className="table-header">
+								<th className="hometeam week-title">Week 4</th>
+								<th className="score">Score</th>
+								<th className="awayteam"></th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td className="hometeam">
+									{teamInfo[6].name +
+										" (" +
+										teamInfo[6].wins +
+										" - " +
+										teamInfo[6].losses +
+										")"}
+								</td>
+								<td className="score">0 - 0</td>
+								<td className="awayteam">
+									{teamInfo[3].name +
+										" (" +
+										teamInfo[3].wins +
+										" - " +
+										teamInfo[3].losses +
+										")"}
+								</td>
+							</tr>
+							<tr>
+								<td className="hometeam">
+									{teamInfo[1].name +
+										" (" +
+										teamInfo[1].wins +
+										" - " +
+										teamInfo[1].losses +
+										")"}
+								</td>
+								<td className="score">0 - 0</td>
+								<td className="awayteam">
+									{teamInfo[7].name +
+										" (" +
+										teamInfo[7].wins +
+										" - " +
+										teamInfo[7].losses +
+										")"}
+								</td>
+							</tr>
+							<tr>
+								<td className="hometeam">
+									{teamInfo[2].name +
+										" (" +
+										teamInfo[2].wins +
+										" - " +
+										teamInfo[2].losses +
+										")"}
+								</td>
+								<td className="score">0 - 0</td>
+								<td className="awayteam">
+									{teamInfo[4].name +
+										" (" +
+										teamInfo[4].wins +
+										" - " +
+										teamInfo[4].losses +
+										")"}
+								</td>
+							</tr>
+							<tr>
+								<td className="hometeam">
+									{teamInfo[0].name +
+										" (" +
+										teamInfo[0].wins +
+										" - " +
+										teamInfo[0].losses +
+										")"}
+								</td>
+								<td className="score">0 - 0</td>
+								<td className="awayteam">
+									{teamInfo[5].name +
+										" (" +
+										teamInfo[5].wins +
+										" - " +
+										teamInfo[5].losses +
+										")"}
+								</td>
+							</tr>
+						</tbody>
+					</table>
+
+					{/*------------------------------------------ WEEK 5 ---------------------------------------------------------------------*/}
+					<table className="styled-table matchup">
+						<thead>
+							<tr className="table-header">
+								<th className="hometeam week-title">Week 5</th>
+								<th className="score">Score</th>
+								<th className="awayteam"></th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td className="hometeam">
+									{teamInfo[6].name +
+										" (" +
+										teamInfo[6].wins +
+										" - " +
+										teamInfo[6].losses +
+										")"}
+								</td>
+								<td className="score">0 - 0</td>
+								<td className="awayteam">
+									{teamInfo[1].name +
+										" (" +
+										teamInfo[1].wins +
+										" - " +
+										teamInfo[1].losses +
+										")"}
+								</td>
+							</tr>
+							<tr>
+								<td className="hometeam">
+									{teamInfo[2].name +
+										" (" +
+										teamInfo[2].wins +
+										" - " +
+										teamInfo[2].losses +
+										")"}
+								</td>
+								<td className="score">0 - 0</td>
+								<td className="awayteam">
+									{teamInfo[3].name +
+										" (" +
+										teamInfo[3].wins +
+										" - " +
+										teamInfo[3].losses +
+										")"}
+								</td>
+							</tr>
+							<tr>
+								<td className="hometeam">
+									{teamInfo[0].name +
+										" (" +
+										teamInfo[0].wins +
+										" - " +
+										teamInfo[0].losses +
+										")"}
+								</td>
+								<td className="score">0 - 0</td>
+								<td className="awayteam">
+									{teamInfo[7].name +
+										" (" +
+										teamInfo[7].wins +
+										" - " +
+										teamInfo[7].losses +
+										")"}
+								</td>
+							</tr>
+							<tr>
+								<td className="hometeam">
+									{teamInfo[4].name +
+										" (" +
+										teamInfo[4].wins +
+										" - " +
+										teamInfo[4].losses +
+										")"}
+								</td>
+								<td className="score">0 - 0</td>
+								<td className="awayteam">
+									{teamInfo[5].name +
+										" (" +
+										teamInfo[5].wins +
+										" - " +
+										teamInfo[5].losses +
+										")"}
+								</td>
+							</tr>
+						</tbody>
+					</table>
+
+					{/*------------------------------------------ WEEK 6 ---------------------------------------------------------------------*/}
+
+					<table className="styled-table matchup">
+						<thead>
+							<tr className="table-header">
+								<th className="hometeam week-title">Week 6</th>
+								<th className="score">Score</th>
+								<th className="awayteam"></th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td className="hometeam">
+									{teamInfo[6].name +
+										" (" +
+										teamInfo[6].wins +
+										" - " +
+										teamInfo[6].losses +
+										")"}
+								</td>
+								<td className="score">0 - 0</td>
+								<td className="awayteam">
+									{teamInfo[7].name +
+										" (" +
+										teamInfo[7].wins +
+										" - " +
+										teamInfo[7].losses +
+										")"}
+								</td>
+							</tr>
+							<tr>
+								<td className="hometeam">
+									{teamInfo[3].name +
+										" (" +
+										teamInfo[3].wins +
+										" - " +
+										teamInfo[3].losses +
+										")"}
+								</td>
+								<td className="score">0 - 0</td>
+								<td className="awayteam">
+									{teamInfo[4].name +
+										" (" +
+										teamInfo[4].wins +
+										" - " +
+										teamInfo[4].losses +
+										")"}
+								</td>
+							</tr>
+							<tr>
+								<td className="hometeam">
+									{teamInfo[1].name +
+										" (" +
+										teamInfo[1].wins +
+										" - " +
+										teamInfo[1].losses +
+										")"}
+								</td>
+								<td className="score">0 - 0</td>
+								<td className="awayteam">
+									{teamInfo[5].name +
+										" (" +
+										teamInfo[5].wins +
+										" - " +
+										teamInfo[5].losses +
+										")"}
+								</td>
+							</tr>
+							<tr>
+								<td className="hometeam">
+									{teamInfo[2].name +
+										" (" +
+										teamInfo[2].wins +
+										" - " +
+										teamInfo[2].losses +
+										")"}
+								</td>
+								<td className="score">0 - 0</td>
+								<td className="awayteam">
+									{teamInfo[0].name +
+										" (" +
+										teamInfo[0].wins +
+										" - " +
+										teamInfo[0].losses +
+										")"}
+								</td>
+							</tr>
+						</tbody>
+					</table>
+					
+					{/*------------------------------------------ WEEK 7 ---------------------------------------------------------------------*/}
+					<table className="styled-table matchup">
+						<thead>
+							<tr className="table-header">
+								<th className="hometeam week-title">Week 7</th>
+								<th className="score">Score</th>
+								<th className="awayteam"></th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td className="hometeam">
+									{teamInfo[6].name +
+										" (" +
+										teamInfo[6].wins +
+										" - " +
+										teamInfo[6].losses +
+										")"}
+								</td>
+								<td className="score">0 - 0</td>
+								<td className="awayteam">
+									{teamInfo[0].name +
+										" (" +
+										teamInfo[0].wins +
+										" - " +
+										teamInfo[0].losses +
+										")"}
+								</td>
+							</tr>
+							<tr>
+								<td className="hometeam">
+									{teamInfo[2].name +
+										" (" +
+										teamInfo[2].wins +
+										" - " +
+										teamInfo[2].losses +
+										")"}
+								</td>
+								<td className="score">0 - 0</td>
+								<td className="awayteam">
+									{teamInfo[5].name +
+										" (" +
+										teamInfo[5].wins +
+										" - " +
+										teamInfo[5].losses +
+										")"}
+								</td>
+							</tr>
+							<tr>
+								<td className="hometeam">
+									{teamInfo[4].name +
+										" (" +
+										teamInfo[4].wins +
+										" - " +
+										teamInfo[4].losses +
+										")"}
+								</td>
+								<td className="score">0 - 0</td>
+								<td className="awayteam">
+									{teamInfo[1].name +
+										" (" +
+										teamInfo[1].wins +
+										" - " +
+										teamInfo[1].losses +
+										")"}
+								</td>
+							</tr>
+							<tr>
+								<td className="hometeam">
+									{teamInfo[7].name +
+										" (" +
+										teamInfo[7].wins +
+										" - " +
+										teamInfo[7].losses +
+										")"}
+								</td>
+								<td className="score">0 - 0</td>
+								<td className="awayteam">
+									{teamInfo[3].name +
+										" (" +
+										teamInfo[3].wins +
+										" - " +
+										teamInfo[3].losses +
+										")"}
+								</td>
+							</tr>
+						</tbody>
 					</table>
 				</motion.div>
 			)}
